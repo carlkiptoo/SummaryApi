9448c2499e8ad677c6e6afe0022fc09a3f1df49c
carlkiptoo <carlosiamcarl@gmail.com>
2024-11-13 12:04:09 +0300

    template complete

--- summaryApp/src/components/SummaryApp.vue
+++ summaryApp/src/components/SummaryApp.vue
@@ -1,5 +1,22 @@
 <template>
-    <div class="document-summarizer">
-        
+  <div class="document-summarizer">
+    <input
+      type="file"
+      @change="handleFileUpload"
+      accept=".pdf"
+      :disabled="isLoading"
+    />
+    <button
+      @click="summarizeDocument"
+      :disabled="!selectedFile || isLoading"
+      class="submit-btn"
+    >
+      {{ isLoading ? "Summarizing..." : "Summarize" }}
+    </button>
+    <div v-if="error" class="error">{{ error }}</div>
+    <div v-if="summary" class="summary">
+      <h3>Summary</h3>
+      <p>{{ summary }}</p>
     </div>
+  </div>
 </template>
